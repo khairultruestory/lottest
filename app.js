cf40c43755ac1eaf252d7df32055d6c1af58835c
TechnicZ Truestory <43160410+khairultruestory@users.noreply.github.com>
2023-12-11 18:20:34 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,115 @@
+const contractAddress = "0xA8500e51A0e44816887a8DFbbcbB230bE73052E1";
+const abi = [
+	{
+		"inputs": [],
+		"name": "enter",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "pickWinner",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "getParticipants",
+		"outputs": [
+			{
+				"internalType": "address payable[]",
+				"name": "",
+				"type": "address[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "manager",
+		"outputs": [
+			{
+				"internalType": "address payable",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "participants",
+		"outputs": [
+			{
+				"internalType": "address payable",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+ 
+let contract;
+let web3;
+let accounts;
+ 
+window.addEventListener('load', async () => {
+    if(window.ethereum) {
+        web3 = new Web3(window.ethereum);
+        try {
+            await window.ethereum.enable();
+            initApp();
+        } catch (error) {
+            console.error("Access to your Ethereum account rejected.");
+        }
+    } else {
+        console.error("Please install MetaMask!");
+    }
+});
+ 
+function initApp() {
+    contract = new web3.eth.Contract(abi, contractAddress);
+ 
+    document.getElementById('connectWallet').addEventListener('click', async () => {
+        accounts = await web3.eth.getAccounts();
+        console.log("Connected account:", accounts[0]);
+    });
+ 
+    document.getElementById('enterLottery').addEventListener('click', () => {
+        contract.methods.enter().send({ from: accounts[0], value: web3.utils.toWei("0.01", "ether") })
+        .then(() => console.log("Entered the lottery!"))
+        .catch(console.error);
+    });
+ 
+    document.getElementById('pickWinner').addEventListener('click', () => {
+        contract.methods.pickWinner().send({ from: accounts[0] })
+        .then(() => console.log("Winner picked!"))
+        .catch(console.error);
+    });
+ 
+    // Fetch and display participants
+    contract.methods.getParticipants().call()
+    .then(displayParticipants)
+    .catch(console.error);
+}
+ 
+function displayParticipants(participants) {
+    const participantsList = document.getElementById('participantsList');
+    participantsList.innerHTML = participants.map(address => `<li>${address}</li>`).join('');}
